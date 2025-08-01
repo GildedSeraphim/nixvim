7cb66989ed3de561df4aa47598956128e806d78b
GildedSeraphim <nathanishaaz@gmail.com>
2025-08-01 17:54:57 -0500

    custom conf

--- modules/nixvim/plug/lsp/default.nix
+++ modules/nixvim/plug/lsp/default.nix
@@ -116,36 +116,47 @@
     };
   };
   extraConfigLua = ''
-    local _border = "rounded"
+      local _border = "rounded"
 
-    vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
-      vim.lsp.handlers.hover, {
+      vim.lsp.handlers["textDocument/hover"] = vim.lsp.with(
+        vim.lsp.handlers.hover, {
+          border = _border
+        }
+      )
+
+      vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
+        vim.lsp.handlers.signature_help, {
+          border = _border
+        }
+      )
+
+      vim.diagnostic.config{
+        float={border=_border}
+      };
+
+      require('lspconfig.ui.windows').default_options = {
         border = _border
       }
-    )
 
-    vim.lsp.handlers["textDocument/signatureHelp"] = vim.lsp.with(
-      vim.lsp.handlers.signature_help, {
-        border = _border
+    local lspconfig = require('lspconfig')
+    local util = require('lspconfig.util')
+
+    -- Only try to start ZLS if it's in $PATH
+    if vim.fn.executable("zls") == 1 then
+      lspconfig.zls.setup {
+        cmd = { "zls" },  -- use the one from PATH (your devShell)
+        root_dir = util.root_pattern("build.zig", ".git"),
       }
-    )
+    end
 
-    vim.diagnostic.config{
-      float={border=_border}
-    };
-
-    require('lspconfig.ui.windows').default_options = {
-      border = _border
-    }
-
-    config = function(_, opts)
-      local lspconfig = require('lspconfig')
-      for server, config in pairs(opts.servers) do
-        -- passing config.capabilities to blink.cmp merges with the capabilities in your
-        -- `opts[server].capabilities, if you've defined it
-        config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
-        lspconfig[server].setup(config)
-      end
-    end;
+      config = function(_, opts)
+        local lspconfig = require('lspconfig')
+        for server, config in pairs(opts.servers) do
+          -- passing config.capabilities to blink.cmp merges with the capabilities in your
+          -- `opts[server].capabilities, if you've defined it
+          config.capabilities = require('blink.cmp').get_lsp_capabilities(config.capabilities)
+          lspconfig[server].setup(config)
+        end
+      end;
   '';
 }
