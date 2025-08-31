53575c94ac698b94fb9e5ae2604178bc2744e88a
GildedSeraphim <nathanishaaz@gmail.com>
2025-08-30 21:52:05 -0500

    completion servers

--- modules/nixvim/plug/blink/default.nix
+++ modules/nixvim/plug/blink/default.nix
@@ -3,6 +3,30 @@
     blink-cmp = {
       enable = true;
       settings = {
+        sources.providers = {
+          latex-symbols = {
+            module = "blink-cmp-latex";
+            name = "Latex";
+            opts = {
+              # set to true to insert the latex command instead of the symbol
+              insert_command = false;
+            };
+          };
+          spell = {
+            module = "blink-cmp-spell";
+            name = "Spell";
+            score_offset = 100;
+            opts = {
+            };
+          };
+        };
+        sources.default = [
+          "lsp"
+          "path"
+          "buffer"
+          "latex-symbols"
+          "spell"
+        ];
         keymap.preset = "super-tab";
         appearance = {
           nerd_font_variant = "mono";
@@ -24,5 +48,6 @@
         };
       };
     };
+
   };
 }
