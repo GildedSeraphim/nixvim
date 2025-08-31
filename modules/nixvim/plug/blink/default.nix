eb9499966dec2f1eb789375c6a541f9000d5c788
GildedSeraphim <nathanishaaz@gmail.com>
2025-08-31 09:43:25 -0500

    fixed blink

--- modules/nixvim/plug/blink/default.nix
+++ modules/nixvim/plug/blink/default.nix
@@ -4,28 +4,12 @@
       enable = true;
       settings = {
         sources.providers = {
-          latex-symbols = {
-            module = "blink-cmp-latex";
-            name = "Latex";
-            opts = {
-              # set to true to insert the latex command instead of the symbol
-              insert_command = false;
-            };
-          };
-          spell = {
-            module = "blink-cmp-spell";
-            name = "Spell";
-            score_offset = 100;
-            opts = {
-            };
-          };
+         
         };
         sources.default = [
           "lsp"
           "path"
           "buffer"
-          "latex-symbols"
-          "spell"
         ];
         keymap.preset = "super-tab";
         appearance = {
